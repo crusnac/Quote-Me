8c5d13b197a03a37831d9e9ba41bfbfa3a4ab8d8
Claudiu Rusnac <claudiu@rusnac.net>
2013-10-16 13:52:44 -0700

    Modified logic.

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -6,6 +6,7 @@ class users_controller extends base_controller {
         parent::__construct();
     } 
 
+
 	########### //Inital Signup Function ###########
 	public function signup(){
 		
@@ -17,6 +18,7 @@ class users_controller extends base_controller {
 		echo $this->template;		
 	}
 	
+	
 	########### //Process Signup Function ###########
 	public function p_signup(){
 	
@@ -27,31 +29,42 @@ class users_controller extends base_controller {
 		//Create a hashed password
 		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
 		
-		
 		//Create a hashed token
 		 $_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
 		
 		
         // Process from _POST parameters and insert them into the DB. 
         $user_id = DB::instance(DB_NAME)->insert('users', $_POST);
-        
-        //Display success message - Add a view for this. Should redirect to the login page probably!
-        echo 'You\'re signed up';
+        		
+		//Redirect to user Profile Page after user has been created in the DB
+        Router::redirect('/users/login');
 	}
 	
-	########### //Initial login function ###########
+	
+	########### //Login function ###########
 	public function login($error = NULL){
+			
+		//Check to see if the user is logged in, if they are, redirect them to the profile page
+		if($this->user) {
+			Router::redirect('/users/profile');
+			
+			//If not logged in, display the login box
+			}else{
+			
+			//Define view parameters
+			$this->template->content = View::instance('v_users_login');
+			$this->template->title   = "Login";
+			
+			//Pass error variable to the view
+			$this->template->content->error = $error;
+			
+			//Display view
+			echo $this->template;		
+			
+			}//End else
 		
-		//Define view paramters
-		$this->template->content = View::instance('v_users_login');
-		$this->template->title   = "Login";
-		
-		// Pass error variable to the view
-		$this->template->content->error = $error;
-		
-		//Display view
-		echo $this->template;		
-	}
+	}// End function
+	
 	
 	########### //Process login function ###########
 	public function p_login(){
@@ -62,11 +75,11 @@ class users_controller extends base_controller {
 		//Set HASH from the form _POST
 		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
 		
-		//Query the DB for a email / password.
-		$q = "SELECT token 
-        	FROM users 
-			WHERE email = '".$_POST['email']."' 
-			AND password = '".$_POST['password']."'"; 
+		//Query the DB for a email / password and set it as a variable.
+		$q = 	"SELECT token 
+        		FROM users 
+				WHERE email = '".$_POST['email']."' 
+				AND password = '".$_POST['password']."'"; 
 			
 		$token = DB::instance(DB_NAME)->select_field($q);   
 
@@ -93,7 +106,53 @@ class users_controller extends base_controller {
 			} // End of else	
 		
 		} //End of function
+		
+		
 	
+		########### //Profile function ###########
+		public function profile(){
+				
+			//Check to see if a user is logged in, if not - redirect to login page.
+			if(!$this->user) {
+				Router::redirect('/users/login');
+				
+				}else{
+		
+				//Define view parameters
+				$this->template->content = View::instance('v_users_profile');
+				$this->template->title = $this->user->first_name .' ' . $this->user->last_name. " | Profile";
+
+				//Display template
+				echo $this->template;
+				
+				} // End of else
+			
+		}//End of function
+		
+		
+		
+		########### //Logout function ###########
+		public function logout(){
+			
+			// Generate a new token for the next login
+			$new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+			
+			//Specify what we are updating = the token
+			$data = Array("token" => $new_token);
+			
+			//Update the with then new token
+			DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+			
+			//Delete exsiting cookie by setting a negative year
+			setcookie("token", "", strtotime('-1 year'), '/');
+			
+			//Redirect to index
+			Router::redirect("/");
+			
+		}//End of function
+
+		
+
 	
 	
 } # end of class
