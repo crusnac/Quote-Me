76f4b11c4ba9eaee282562a9572334671ca24166
Claudiu Rusnac <claudiu@rusnac.net>
2013-10-18 15:36:56 -0700

    Added error messaging

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,21 +1,21 @@
 <?php if(isset($_GET['login-error'])): ?>
 <div class="alert alert-danger fade in">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        <strong>I am unable to log you in.</strong>  Please try again!
+        <strong><i class="icon-warning-sign"></i> I am unable to log you in.</strong>  Please try again!
 </div>
 <?php endif; ?>
 
 <?php if(isset($_GET['user-exists'])): ?>
 <div class="alert alert-danger fade in">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        <strong>That user already exists! </strong>  You can login using your existing account or <a class="alert-link" href="/users/signup/">sign up</a> as a new user.
+        <strong><i class="icon-warning-sign"></i> That user already exists! </strong>  You can login using your existing account or <a class="alert-link" href="/users/signup/">sign up</a> as a new user.
 </div>
 <?php endif; ?>
 
 <?php if(isset($_GET['user-created'])): ?>
 <div class="alert alert-success fade in">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        <strong>Your user has been created!</strong>  Login to continue. 
+        <strong><i class="icon-warning-sign"></i> Your user has been created!</strong>  Login to continue. 
 </div>
 <?php endif; ?>
 
@@ -26,11 +26,13 @@
 
 <form class="form-signin" method="POST" action='/users/p_login'>
 
-	<h2 class="form-signin-heading">Please sign in</h2>
+	<h2 class="form-signin-heading">Please Sign In</h2>
     
+    <label><small>Your Email Address</small></label>
     <input type="text" name="email" class="form-control" placeholder="Email address" autofocus="">
-    <input type="password" name="password" class="form-control" placeholder="Password">
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <label><small>Your Password</small></label>
+    <input type="password" name="password" class="form-control" placeholder="Password"><br />
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
 
 </form>
 
