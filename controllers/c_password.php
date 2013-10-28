55b41feadce78db530d1a9c6d54d8df9dd03f898
Claudiu Rusnac <claudiu@rusnac.net>
2013-10-27 19:43:05 -0700

    a

--- controllers/c_password.php
+++ controllers/c_password.php
@@ -75,7 +75,7 @@ class password_controller extends base_controller {
 							
 						$body = View::instance('v_email_p_reset');
 
-						$this->template->token = $passwordToken;
+						$this->template->content->token = $passwordToken;
 							
 						# Send email
 						Email::send($to, $from, $subject, $body, true, '');
