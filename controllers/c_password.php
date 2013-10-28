02d86b34cff6f252f8691b76d8de9af1dcde264c
Claudiu Rusnac <claudiu@rusnac.net>
2013-10-27 19:22:24 -0700

    Changed token to view

--- controllers/c_password.php
+++ controllers/c_password.php
@@ -78,7 +78,7 @@ class password_controller extends base_controller {
 							
 						$body = View::instance('v_email_p_reset');
 
-						$this->template->content->token = $passwordToken;
+						$this->template->token = $passwordToken;
 							
 						# Send email
 						Email::send($to, $from, $subject, $body, true, '');
