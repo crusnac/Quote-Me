cbcfefc6d23d8429cd7632a694a4ba7296c021e3
Claudiu Rusnac <claudiu@rusnac.net>
2013-10-29 12:16:13 -0700

    Added character counts in the edit posts

--- views/v_posts_edit.php
+++ views/v_posts_edit.php
@@ -1,13 +1,33 @@
+<script type="text/javascript">
+    $(function () {
+        $('textarea.limited').maxlength({
+            'feedback' : '.charsLeft' // note: looks within the current form
+        });
+        
+        $('input.limited').maxlength({
+            'feedback' : '.charsLeftInput' // note: looks within the current form
+        });
+     
+    });
+    
+</script>
+
+
 <div class="jumbotron">
 
 	<?php foreach ($posts as $post): ?>
 		<form method='POST' action='/posts/p_edit/<?php echo $post['id']; ?>'>
 		
 		  	<label><small>Quote Title</small></label>
-		    <input type="text" name="title" class="form-control" value="<?php echo $post['title']; ?>" placeholder="Your Quote Title" autofocus="">
+		    <input type="text" name="title" maxlength="100" class="form-control limited" value="<?php echo $post['title']; ?>" placeholder="Your Quote Title" autofocus="">
+		    <p class="char-count"><strong>Characters left:</strong> <span class="charsLeftInput">10</span></small></p>
+
 		
 		    <label><small>Your Quote</small></label>
-		    <textarea class="form-control" rows="4" cols="50" name="content"><?php echo $post['content']; ?></textarea><br />
+		    <textarea class="form-control limited" maxlength="250" rows="4" cols="50" name="content"><?php echo $post['content']; ?></textarea>
+		    <p class="char-count"><strong>Characters left:</strong> <span class="charsLeft">10</span></small></p>
+
+		    <br />
 		
 		    <button class="btn btn-lg btn-primary btn-block" type="submit">Update My Quote!</button>	
 		</form>
